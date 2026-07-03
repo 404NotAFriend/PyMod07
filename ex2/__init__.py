75451eb0bea65e398731ca779dc0365c4e5f2778
404NotAFriend <brunodrcgomes@gmail.com>
2026-07-03 21:43:47 +0100

    created strategy.py and init for ex2 , corrected flake and mypy

---
+++ ex2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+from .strategy import BattleStrategy
+
+
+__all__ = ["BattleStrategy", "NormalStrategy",
+           "AggressiveStrategy", "DefensiveStrategy"]
---
+++ ex2/strategy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+
+from abc import ABC, abstractmethod
+from ex0.creature import Creature
+from ex1.capabilities import TransformCapability, HealCapability
+from typing import cast
+
+
+class BattleStrategy(ABC):
+    @abstractmethod
+    def is_valid(self, creature: Creature) -> bool:
+        pass
+
+    @abstractmethod
+    def act(self, creature: Creature) -> list[str]:
+        pass
+
+
+class InvalidStrategyError(Exception):
+    pass
+
+
+class NormalStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return True
+
+    def act(self, creature: Creature) -> list[str]:
+        return [creature.attack()]
+
+
+class AggressiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return isinstance(creature, TransformCapability)
+
+    def act(self, creature: Creature) -> list[str]:
+        if not self.is_valid(creature):
+            raise InvalidStrategyError(
+                f"Invalid Creature '{creature.name}'"
+                " for this aggressive strategy")
+        transformation = cast(TransformCapability, creature)
+        return [transformation.transform(),
+                creature.attack(),
+                transformation.revert()]
+
+
+class DefensiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return isinstance(creature, HealCapability)
+
+    def act(self, creature: Creature) -> list[str]:
+        if not self.is_valid(creature):
+            raise InvalidStrategyError(
+                f"Invalid Creature '{creature.name}'"
+                " for this defensive strategy")
+        healing = cast(HealCapability, creature)
+        return [creature.attack(), healing.heal("small")]
